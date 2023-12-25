2446326addd6d93afb5269181092dc8dc42222f6
Blake Skor <blake.skor@outlook.com>
2023-12-25 17:45:19 -0500

    new ui and post content fix

--- public/scripts/notes.js
+++ public/scripts/notes.js
@@ -1,54 +1,71 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const sidebar = document.getElementById("sidebar");
-	const noteContent = document.getElementById("noteContent");
+	const editNoteTitle = document.getElementById("editNoteTitle");
+	const editNoteContent = document.getElementById("editNoteContent");
+	const noteContentArea = document.getElementById("noteContent");
 
-	fetch("/api/notes")
-		.then((response) => response.json())
-		.then((notes) => {
-			notes.forEach((note) => {
-				const btn = document.createElement("button");
-				btn.classList.add("btn", "btn-outline-primary", "w-100", "my-1");
-				btn.textContent = note.title;
-				btn.onclick = () => displayNote(note);
-				sidebar.appendChild(btn);
-			});
-		})
-		.catch((error) => console.error("Error fetching notes:", error));
-
-	function displayNote(note) {
-		noteContent.innerHTML = `<h3>${note.title}</h3><p>${note.content}</p>`;
+	function displayDefaultHeading() {
+		noteContentArea.innerHTML = "<h3>Select a Note to Get Started</h3>";
 	}
+
+	displayDefaultHeading();
+
+	function loadNotes() {
+		fetch("/api/notes")
+			.then((response) => response.json())
+			.then((notes) => {
+				sidebar.innerHTML = "";
+				notes.forEach((note) => {
+					const btn = document.createElement("button");
+					btn.classList.add("btn", "btn-outline-primary", "w-100", "my-1");
+					btn.textContent = note.title;
+					btn.onclick = () => {
+						editNoteTitle.value = note.title;
+						editNoteContent.value = note.content;
+
+						noteContentArea.innerHTML = `
+													<input type="text" value="${note.title}" id="editNoteTitle" class="form-control mb-2" placeholder="Note Title">
+													<textarea id="editNoteContent" class="form-control mb-2" rows="10" placeholder="Note Content">${note.content}</textarea>
+													<button id="saveNoteBtn" class="btn btn-primary">Save</button>
+											`;
+					};
+					sidebar.appendChild(btn);
+				});
+			})
+			.catch((error) => console.error("Error loading notes:", error));
+	}
+
+	loadNotes();
 
 	document
 		.getElementById("newNoteForm")
 		.addEventListener("submit", function (event) {
 			event.preventDefault();
 
-			const title = document.getElementById("noteTitle").value;
-			const content = document.getElementById("noteContent").value;
-
-			const noteData = {
-				title: title,
-				content: content,
-			};
+			const title = document.getElementById("newNoteTitle").value;
+			const content = document.getElementById("newNoteContent").value;
 
 			fetch("/api/notes", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
 				},
-				body: JSON.stringify(noteData),
+				body: JSON.stringify({title, content}),
 			})
 				.then((response) => response.json())
 				.then((data) => {
-					console.log("Success:", data);
-					bootstrap.Modal.getInstance(
+					console.log("Note added:", data);
+					loadNotes();
+
+					const newNoteModal = bootstrap.Modal.getInstance(
 						document.getElementById("newNoteModal")
-					).hide();
+					);
+					newNoteModal.hide();
+
 					document.getElementById("newNoteForm").reset();
 				})
 				.catch((error) => {
-					console.error("Error:", error);
+					console.error("Error adding note:", error);
 				});
 		});
 });
