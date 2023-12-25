e455968d24e89decec6dd83dabf423cf085e0add
Blake Skor <blake.skor@outlook.com>
2023-12-24 20:34:06 -0500

    added get/post note

--- server.js
+++ server.js
@@ -32,14 +32,29 @@ app.get("/notes", (req, res) => {
 
 // The following HTML routes should be created:
 
-// GET /notes should return the notes.html file.
-
-// GET * should return the index.html file.
-
 // The following API routes should be created:
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => {
+	try {
+		res.sendFile(path.join(__dirname, "db/db.json"));
+	} catch (err) {
+		res.json(err);
+	}
+});
 
+app.post("/api/notes", (req, res) => {
+	try {
+		console.log(req.body);
+		const note = {
+			title: req.body.title,
+			content: req.body.content,
+		};
+		res.send(note);
+	} catch (err) {
+		res.json(err);
+	}
+});
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.listen(PORT, () => {
