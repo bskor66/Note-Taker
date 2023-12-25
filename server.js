76b27596ffd16fd303e36a51064b6fded6ee6095
Blake Skor <blake.skor@outlook.com>
2023-12-24 20:14:32 -0500

    serve notes.html from pages

--- server.js
+++ server.js
@@ -10,11 +10,14 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+app.get("/notes", (req, res) => {
+	res.sendFile(path.join(__dirname, "/public/pages/notes.html"));
+});
+// WHEN I click on the link to the notes page
 // THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
 // WHEN I enter a new note title and the note’s text
 // THEN a "Save Note" button and a "Clear Form" button appear in the navigation at the top of the page
